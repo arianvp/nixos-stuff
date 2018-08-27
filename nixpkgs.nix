d6a5a6e7f66f7a4735c92c35916fb4388c10517d
Arian van Putten <aeroboy94@gmail.com>
2018-08-27 11:52:43 +0200

    Add customized version of nixpkgs

---
+++ nixpkgs.nix
@@ -0,0 +1,16 @@
+{ nixpkgs ? import <nixpkgs> }
+let
+  isDir = path: builtins.pathExists (path + "/.");
+  overlays = path:
+    if isDir path 
+    then
+      let
+        content = builtins.readDir path;
+      in
+        map (n: import (path + ("/" + n)))
+            (builtins.filter (n: builtins.match ".*\\.nix" n != null || builtins.pathExists (path + ("/" + n + "/default.nix")))
+                    (builtins.attrNames content))
+    else
+      import path;
+in
+  import nixpkgs { overlays = overlays ./overlays; }
