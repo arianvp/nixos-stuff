9d48bfa84560b725bf63a2fcb8345230fccff133
Arian van Putten <aeroboy94@gmail.com>
2018-09-04 13:57:54 +0200

    Update neovim

--- overlays/neovim.nix
+++ overlays/neovim.nix
@@ -2,12 +2,18 @@ self: super: {
   neovim = super.neovim.override {
     configure = {
       customRC = ''
-      set expandtab
-      set shiftwidth=2
-      set softtabstop=2
+        set expandtab
+        set shiftwidth=2
+        set softtabstop=2
+        set mouse=a
+        map <C-p> :FZF
+        map <C-a> :Ag 
+        map <C-n> :NERDTreeToggle<CR>
+        map <C-t> :TagbarToggle<CR>
+        let g:ctrlp_user_command = ['.git/', 'git --git-dir=%s/.git ls-files -oc --exclude-standard']
       '';
       packages.myPackages = with self.vimPlugins; {
-        start = [ fugitive fzfWrapper fzf-vim  vim-nix ];
+        start = [ tagbar nerdtree fugitive fzfWrapper fzf-vim  vim-nix ];
       };
     };
   };
