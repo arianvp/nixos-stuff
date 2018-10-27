930d9c5544a368e76546c9e910c35337d19d6402
Arian van Putten <aeroboy94@gmail.com>
2018-10-27 22:45:53 +0200

    Add monitoring

---
+++ computers/arianvp.me/monitoring.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, ... }:
+  {
+    config = {
+      services.prometheus = {
+        enable = true;
+        scrapeConfigs = [
+          {
+            job_name = "static";
+            static_configs = [
+              {
+                targets = [
+                  "localhost:${toString (config.services.prometheus.exporters.node.port)}"
+                ];
+              }
+            ];
+          }
+        ];
+        exporters.node.enable = true;
+        rules = [
+          ''
+            ALERT DiskFullInTwoDays 
+            IF (predict_linear(node_filesystem_free[1d], 3600*24*2) < 0) 
+            FOR 5m
+            ANNOTATIONS {
+              summary = "Dun Goofed"
+            }
+            
+          ''
+        ];
+      };
+    };
+  }
