e952da1e4dec11d8e0bed8aebfc7c001ae984e9f
Arian van Putten <aeroboy94@gmail.com>
2018-10-27 18:38:28 +0200

    Add rightfold to the server and add minecraft backups
    Polkit rule allows rightfold to trigger the backup manually

--- computers/arianvp.me/default.nix
+++ computers/arianvp.me/default.nix
@@ -7,6 +7,7 @@
     device = "/dev/vda";
   };
   nixpkgs.config.allowUnfree = true;
+
   nixpkgs.overlays = [
     (self: super: {
       minecraft-server = super.minecraft-server.overrideAttrs (_: rec {
@@ -26,6 +27,36 @@
     openFirewall = true;
   };
 
+  systemd.services.minecraft-backup = {
+    serviceConfig.Type = "oneshot";
+    script = ''
+      MCBACKUPS='${config.users.users.rightfold.home}'
+      export PATH='${pkgs.gzip}/bin:${pkgs.gnutar}/bin:${pkgs.coreutils}/bin'
+      tar cz -C '${config.services.minecraft-server.dataDir}' . > "$MCBACKUPS/world-$(date -Iseconds).tar.gz"
+    '';
+  };
+  systemd.timers.minecraft-backup = {
+    timerConfig.OnCalendar =  "*-*-* 4:00:00";
+    wantedBy = [ "timers.target" ];
+  };
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      polkit.log("action=" + subject);
+      polkit.log("subject=" + subject);
+      if (action.id == "org.freedesktop.systemd1.manage-units") {
+        if (action.lookup("unit") == "minecraft-backup.service") {
+          var verb = action.lookup("verb");
+          if (verb == "start" || verb == "stop" || verb == "restart") {
+            if (subject.user == "rightfold") {
+              return polkit.Result.YES;
+            }
+          }
+        }
+      }
+    });
+  '';
+
   services.openssh = {
     enable = true;
     passwordAuthentication = false;
@@ -44,6 +75,18 @@
       sha256 = "0v6hsr6xcpw7b5cdlb312jm4jy1dahqll8v8ppgcbrfgpxp5gmm6";
     })
   ];
+
+  users.users.rightfold = {
+    createHome = true;
+    isNormalUser = true;
+    openssh.authorizedKeys.keyFiles = [
+      (pkgs.fetchurl {
+        url = "https://github.com/rightfold.keys"; 
+        sha256 = "0q6zidm6cpqvpcbgs82v006d3i3qla9040fv65jjmyh9z4lq92m1";
+      })
+    ];
+  };
+
 
 
 
