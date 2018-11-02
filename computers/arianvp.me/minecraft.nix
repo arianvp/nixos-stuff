7d52c5c9c1e3e75e5486beea6d2568f96f04164e
Arian van Putten <aeroboy94@gmail.com>
2018-11-02 22:08:42 +0100

    Fix bug where we downgraded minecraft

--- computers/arianvp.me/minecraft.nix
+++ computers/arianvp.me/minecraft.nix
@@ -1,21 +1,5 @@
 { config, pkgs, ... }:
   {
-    nixpkgs.config.allowUnfree = true;
-    nixpkgs.overlays = [
-      (self:
-        super:
-          {
-            minecraft-server = super.minecraft-server.overrideAttrs (_:
-              rec {
-                name = "minecraft-server-${version}";
-                version = "1.13.1";
-                src = super.fetchurl {
-                  url = "https://launcher.mojang.com/v1/objects/fe123682e9cb30031eae351764f653500b7396c9/server.jar";
-                  sha256 = "1lak29b7dm0w1cmzjn9gyix6qkszwg8xgb20hci2ki2ifrz099if";
-                };
-              });
-          })
-    ];
     services.minecraft-server = {
       enable = true;
       openFirewall = true;
