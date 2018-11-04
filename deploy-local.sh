2b86f3c144f2222af8e5945d499abea7957c6540
Arian van Putten <aeroboy94@gmail.com>
2018-11-04 13:52:47 +0100

    Fix deploy scripts

--- deploy-local.sh
+++ deploy-local.sh
@@ -3,6 +3,8 @@ set -e
 
 target="ryzen"
 result=$(nix-build ./nixpkgs.nix --no-out-link -A "deployments.${target}.toplevel")
+ssh "root@$targetHost" nix-env -p "$profile" --set "$result"
 sudo "${result}/bin/switch-to-configuration" switch
+./setup-user-env.sh
 
 
--- deploy-remote.sh
+++ deploy-remote.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 set -e
 
+# tmpDir=$(mktemp -t -d nixos-rebuild.XXXXXX)
+# SSHOPTS="$NIX_SSHOPTS -o ControlMaster=auto -o ControlPath=$tmpDir/ssh-%n -o ControlPersist=60"
+
+# cleanup() {
+#     for ctrl in "$tmpDir"/ssh-*; do
+#         ssh -o ControlPath="$ctrl" -O exit dummyhost 2>/dev/null || true
+#     done
+#     rm -rf "$tmpDir"
+# }
+# trap cleanup EXIT
+
+
 target="arianvp-me"
 targetHost="arianvp.me"
+profile=/nix/var/nix/profiles/system
 result=$(nix-build ./nixpkgs.nix --no-out-link -A "deployments.${target}.toplevel")
 # nix sign-paths --recursive --key-file "${keyFile}" "${result}"
-nix copy --no-check-sigs --to "ssh://root@${targetHost}" "${result}"
-ssh "root@$targetHost" "${result}/bin/switch-to-configuration switch"
+nix copy --no-check-sigs --to "ssh://root@$targetHost" "$result"
+ssh "root@$targetHost" nix-env -p "$profile" --set "$result"
+ssh "root@$targetHost" "$result/bin/switch-to-configuration" switch
 
 
