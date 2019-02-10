364a5b8da423d640ecfb5781a58b7a9331373a5c
Arian van Putten <aeroboy94@gmail.com>
2019-02-10 21:40:06 +0100

    Add target to deploy-local

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 set -x
-
-target="ryzen"
+sudo nix-channel --update
+target="$1"
 profile=/nix/var/nix/profiles/system
-result=$(nix-build ./nixpkgs.nix --no-out-link -A "deployments.${target}.toplevel")
+result=$(nix-build ./nixpkgs.nix --no-out-link -A "deployments.${target}.toplevel" --show-trace)
 sudo nix-env -p "$profile" --set "$result"
 sudo "${result}/bin/switch-to-configuration" switch
 ./setup-user-env.sh
