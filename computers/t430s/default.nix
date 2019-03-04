293aa02546fa87ef8cedc4943e9fa2655857f027
Arian van Putten <aeroboy94@gmail.com>
2019-03-04 17:40:18 +0100

    Set environment

--- computers/t430s/default.nix
+++ computers/t430s/default.nix
@@ -1,32 +1,31 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: 
+{
   imports = [
     ./hardware-configuration.nix 
    ../../modules/yubikey
    ../../modules/ssh-tweaks.nix
-  ];
-  time.timeZone = "Europe/Amsterdam";
-  programs.bash.enableCompletion = true;
-  hardware.pulseaudio.enable = true;
-  users.users.root = {
-    openssh.authorizedKeys.keyFiles = [ (builtins.fetchurl "https://github.com/arianvp.keys" )];
-  };
-  users.users.arian = {
-    isNormalUser = true;
-    createHome = true;
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keyFiles = [ (builtins.fetchurl "https://github.com/arianvp.keys" )];
-  };
-  environment.gnome3.excludePackages = with pkgs.gnome3; [ gnome-software ];
-  services.xserver =  {
-    enable = true;
-    desktopManager.gnome3 = {
-      enable = true;
+   ../../modules/env.nix
+ ];
+  config = {
+    time.timeZone = "Europe/Amsterdam";
+    programs.bash.enableCompletion = true;
+    hardware.pulseaudio.enable = true;
+    users.users.arian = {
+      isNormalUser = true;
+      createHome = true;
+      extraGroups = [ "wheel" ];
     };
-    displayManager.gdm.enable = true;
+    environment.gnome3.excludePackages = with pkgs.gnome3; [ gnome-software ];
+    services.xserver =  {
+      enable = true;
+      desktopManager.gnome3 = {
+        enable = true;
+      };
+      displayManager.gdm.enable = true;
+    };
+    services.netdata.enable = true;
+    networking.hostName = "t430s";
+    services.sshd.enable = true;
+    system.stateVersion = "18.03"; 
   };
-
-  services.netdata.enable = true;
-  networking.hostName = "t430s";
-  services.sshd.enable = true;
-  system.stateVersion = "18.03"; 
 }
---
+++ modules/env.nix
@@ -0,0 +1,3 @@
+{ ... }: {
+  environment.variables.EDITOR = "nvim";
+}
