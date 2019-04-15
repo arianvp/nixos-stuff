68e9082a12e67fc695147bff169b620049afb087
Arian van Putten <aeroboy94@gmail.com>
2019-04-15 12:52:16 +0200

    Add digitalocean image to deployments

--- overlays/deployments.nix
+++ overlays/deployments.nix
@@ -3,5 +3,6 @@ self: super: {
     "arianvp-me" = super.nixos (import ../computers/arianvp.me);
     "ryzen" =  super.nixos (import ../computers/ryzen);
     "t430s" = super.nixos (import ../computers/t430s);
+    "image" = super.nixos (import ../modules/digitalocean/image.nix );
   };
 }
