910058d639102cfee93f300db068abe5c7acf062
Arian van Putten <aeroboy94@gmail.com>
2019-04-16 12:41:36 +0200

    Finish migration to digitalocean. forgot to open some ports

--- computers/arianvp.me/default.nix
+++ computers/arianvp.me/default.nix
@@ -8,6 +8,12 @@
 
   # Weechat
   services.weechat.enable = true;
+  networking.firewall.allowedTCPPorts = [ 
+    80   # http
+    443  # https
+    4443 # weechat relay
+  ];
+  
   programs.screen.screenrc = ''
     multiuser on
     acladd normal_user
--- overlays/deployments.nix
+++ overlays/deployments.nix
@@ -1,7 +1,7 @@
 self: super: {
   deployments = {
-    "arianvp.me" = super.nixos (import ../computers/arianvp.me.bak);
-    "new.arianvp.me" = super.nixos (import ../computers/arianvp.me);
+    "old.arianvp.me" = super.nixos (import ../computers/arianvp.me.bak);
+    "arianvp.me" = super.nixos (import ../computers/arianvp.me);
     "ryzen" =  super.nixos (import ../computers/ryzen);
     "t430s" = super.nixos (import ../computers/t430s);
   };
