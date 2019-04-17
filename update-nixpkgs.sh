10be85a393d38959bbe62518223b8b7bd3641a2f
Arian van Putten <aeroboy94@gmail.com>
2019-04-18 01:10:23 +0200

    Add borderline insane alternative to nix-channel

---
+++ update-nixpkgs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git ls-remote -h https://github.com/nixos/nixpkgs-channels  | column --table-columns rev,ref --json -d | jq '.table | map(.["value"] = (. | .url = "https://github.com/nixos/nixpkgs-channels") | .["key"] = (.ref | split("/")[2]) | del (.rev, .ref)) | from_entries' > nixpkgs.json
