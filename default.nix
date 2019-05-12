91183d3c6ff621c7de599276fee218076d3340ba
Arian van Putten <aeroboy94@gmail.com>
2019-05-12 13:46:49 +0200

    Start on refactor

--- default.nix
+++ default.nix
@@ -1,19 +1,15 @@
 let 
-  # nixpkgs = import <nixpkgs>; # TODO make this a reproducible version
   channels = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
-  metadata = channels."nixos-19.03";
-  nixpkgs = import (builtins.fetchGit metadata);
-
-  isDir = path: builtins.pathExists (path + "/.");
-  overlays = path:
-    if isDir path 
-    then
-      let
-        content = builtins.readDir path;
-      in
-        map (n: import (path + ("/" + n)))
-            (builtins.filter (n: builtins.match ".*\\.nix" n != null || builtins.pathExists (path + ("/" + n + "/default.nix")))
-                    (builtins.attrNames content))
-    else
-    import path;
-in nixpkgs { overlays = overlays ./overlays; config = { allowUnfree = true; }; }
+  channel = channels."nixos-19.03";
+  nixpkgs = import (builtins.fetchGit channel);
+in 
+  nixpkgs { 
+    overlays = map (n: import n) [
+      ./overlays/deployments.nix
+      ./overlays/neovim.nix
+      ./overlays/user-environment.nix
+    ];
+    config = { 
+      allowUnfree = true; 
+    }; 
+  }
