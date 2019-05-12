92cdfb6eae701e70782dba134436e443ad4f325b
Arian van Putten <aeroboy94@gmail.com>
2019-05-12 13:30:28 +0200

    Add nix-shell to update-nixpkgs.sh script

--- update-nixpkgs.sh
+++ update-nixpkgs.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell -p jq git
 git ls-remote -h https://github.com/nixos/nixpkgs-channels  | column --table-columns rev,ref --json -d | jq '.table | map(.["value"] = (. | .url = "https://github.com/nixos/nixpkgs-channels") | .["key"] = (.ref | split("/")[2]) | del (.rev, .ref)) | from_entries' > nixpkgs.json
