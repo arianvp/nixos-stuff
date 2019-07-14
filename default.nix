9205eb0f442a0e201e672591ad0762a3a651bf16
Arian van Putten <arian.vanputten@gmail.com>
2019-07-14 15:46:40 +0200

    Stuff

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 let 
   channels_ = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+  channel = channels."nixos-19.03";
   channels  = builtins.mapAttrs (k: v: import (builtins.fetchGit v) {
     overlays = map (n: import n) [
       ./deployments.nix
@@ -12,4 +13,4 @@ let
 
   }) channels_;
 in 
-  channels."nixos-19.03";
+  channels."nixos-19.03"
