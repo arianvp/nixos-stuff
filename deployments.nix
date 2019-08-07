1c98d963dd7bfbd1f272cdb818731fec4a229b90
Arian van Putten <arian.vanputten@gmail.com>
2019-08-07 12:28:55 +0200

    Make sure that NIX_PATH points to our pinned nixpkgs
    This makes all the nix tooling work with our pinned stuff.
    
    With the exception from nix-env as it uses this weird .nix-defexpr
    things which I don't understand. But heck, I never use nix-env anyway

--- deployments.nix
+++ deployments.nix
@@ -1,11 +1,13 @@
-self: super: {
+self: super: 
+  # makes sure that all nix commands use our pinned nixpkgs
+  let config = x: { imports = [ x ]; config.nix.nixPath = [ "nixpkgs=${self.path}" ]; }; in {
   deployments = {
-    "old.arianvp.me" = super.nixos (import ./configs/arianvp.me.bak);
-    "arianvp.me" = super.nixos (import ./configs/arianvp.me);
-    "ryzen" =  super.nixos (import ./configs/ryzen);
-    "t430s" = super.nixos (import ./configs/t430s);
-    "t490s" = super.nixos (import ./configs/t490s);
+    "old.arianvp.me" = super.nixos (config ./configs/arianvp.me.bak);
+    "arianvp.me" = super.nixos (config ./configs/arianvp.me);
+    "ryzen" =  super.nixos (config ./configs/ryzen);
+    "t430s" = super.nixos (config ./configs/t430s);
+    "t490s" = super.nixos (config ./configs/t490s);
   };
-  digitalocean-image = (super.nixos (import ./modules/digitalocean/image.nix )).digitalOceanImage;
+  digitalocean-image = (super.nixos (config ./modules/digitalocean/image.nix )).digitalOceanImage;
   arianvp-website = ./website;
 }
