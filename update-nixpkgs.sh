de83314327641df32b19c260794823074cd08af3
Arian van Putten <arian.vanputten@gmail.com>
2019-08-07 15:01:07 +0200

    Don't use nix-shell anymore. it's old cruft

--- update-nixpkgs.sh
+++ update-nixpkgs.sh
@@ -1,3 +1,1 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p jq git
-git ls-remote -h https://github.com/nixos/nixpkgs-channels  | column --table-columns rev,ref --json -d | jq '.table | map(.["value"] = (. | .url = "https://github.com/nixos/nixpkgs-channels") | .["key"] = (.ref | split("/")[2]) | del (.rev, .ref)) | from_entries' > nixpkgs.json
+nix run nixpkgs.jq nixpkgs.git -c bash -c  "git ls-remote -h https://github.com/nixos/nixpkgs-channels  | column --table-columns rev,ref --json -d | jq '.table | map(.[\"value\"] = (. | .url = \"https://github.com/nixos/nixpkgs-channels\") | .[\"key\"] = (.ref | split(\"/\")[2]) | del (.rev, .ref)) | from_entries' > nixpkgs.json"
