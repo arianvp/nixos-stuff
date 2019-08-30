0c78d2951979e32bb903b9beb1ebe6ed17a0aac9
Arian van Putten <arian.vanputten@gmail.com>
2019-08-30 14:47:50 +0200

    Make deploy.sh be generated, instead of it calling things

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 # Usage
 ```
-$ ./deploy.sh <deployment> <target> [switch|boot|kexec]
+nix run -f . deployments.t490s -c deploy [switch|boot|test] [target*]
+```
+
+# Example
+```
+nix run -f . deployments.arianvp-me -c deploy switch root@arianvp.me
 ```
 
 # Deployments
@@ -18,7 +23,7 @@ nix-build ./nixpkgs.nix -A digitalocean-image
 
 2. Upload it to Digitalocean
 3. Start a droplet
-4. you can now deploy with `./deploy.sh <deployment> root@<droplet ip> switch`
+4. you can now deploy with `nix run -f . deployments.arianvp-me -c deploy switch root@<droplet ip>`
 
 
 # Setting up user environment
--- deployments.nix
+++ deployments.nix
@@ -1,13 +1,48 @@
 self: super: 
   # makes sure that all nix commands use our pinned nixpkgs
-  let config = x: { imports = [ x ]; config.nix.nixPath = [ "nixpkgs=${./.}" ]; }; in {
-  deployments = {
-    "old.arianvp.me" = super.nixos (config ./configs/arianvp.me.bak);
-    "arianvp.me" = super.nixos (config ./configs/arianvp.me);
-    "ryzen" =  super.nixos (config ./configs/ryzen);
-    "t430s" = super.nixos (config ./configs/t430s);
-    "t490s" = super.nixos (config ./configs/t490s);
-  };
-  digitalocean-image = (super.nixos (config ./modules/digitalocean/image.nix )).digitalOceanImage;
-  arianvp-website = ./website;
+  let 
+    # TODO: filterSource the nixPath for unwanted rebuilds
+    config = x: { imports = [ x ]; config.nix.nixPath = [ "nixpkgs=${./.}" ]; }; 
+    deploy = deployment: super.writeScriptBin "deploy" 
+    ''
+      set -e
+      tmpDir=$(mktemp -t -d nixos-rebuild.XXXXXX)
+      SSHOPTS="$NIX_SSHOPTS -o ControlMaster=auto -o ControlPath=$tmpDir/ssh-%n -o ControlPersist=60"
+      cleanup() {
+          for ctrl in "$tmpDir"/ssh-*; do
+              ssh -o ControlPath="$ctrl" -O exit dummyhost 2>/dev/null || true
+          done
+          rm -rf "$tmpDir"
+      }
+      trap cleanup EXIT
+
+      profile=/nix/var/nix/profiles/system
+      action="$1"
+      remote="''${2:-local}"
+
+      if [ "$remote" != "local" ]; then
+        store="ssh://$remote"
+        NIX_SSHOPTS=$SSHOPTS nix copy --no-check-sigs --to "$store" "${deployment.toplevel}"
+      fi
+
+      remoteOrLocal() {
+        if [ "$remote" == "local" ]; then 
+          "$@"
+        else
+          ssh $SSHOPTS "$remote" "$@"
+        fi
+      }
+      remoteOrLocal sudo nix-env --profile "$profile" --set "${deployment.toplevel}" --show-trace
+      remoteOrLocal sudo "$profile/bin/switch-to-configuration" "$action"
+    '';
+  in {
+    deployments = {
+      "old.arianvp.me" = deploy (super.nixos (config ./configs/arianvp.me.bak));
+      "arianvp-me" = deploy (super.nixos (config ./configs/arianvp.me));
+      "ryzen" =  deploy (super.nixos (config ./configs/ryzen));
+      "t430s" = deploy (super.nixos (config ./configs/t430s));
+      "t490s" = deploy (super.nixos (config ./configs/t490s));
+    };
+    digitalocean-image = (super.nixos (config ./modules/digitalocean/image.nix )).digitalOceanImage;
+    arianvp-website = ./website;
 }
