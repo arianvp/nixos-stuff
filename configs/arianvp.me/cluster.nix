6febedfd654bb20a211beecf4e412a94ad62b4a0
Arian van Putten <arian.vanputten@gmail.com>
2019-11-03 17:03:41 +0100

    Remove mattermost. Move to optional module

---
+++ configs/arianvp.me/cluster.nix
@@ -0,0 +1,35 @@
+let 
+  token = "wi1tll.gham5k7iy47gs8od";
+  zone = "cluster";
+  /*Network = {
+    "70-container-host0" = {
+      Match = { Virtualization = "container"; Name = "host0"; }; 
+      IPV6AcceptRA = "yes";
+    }; 
+    "70-container-vz" = {
+      Match = { Driver = "bridge"; Name = "vz-*"; };
+      # I don't need an address I think because I'm not masquerading?
+      IPForward = "ipv6";
+      IPv6PrefixDelegation = "static"; 
+      IPv6Prefix = [
+        { Prefix = "fc00::feef::feef::/64" };  # needs to be a public prefix
+      ];
+    };
+  };*/
+in {
+  master1 = { ... }: {
+    systemd.network.networks."80-container-host0" = {
+      networkConfig.Address = "fc00::feef::feef::1/64";
+    };
+    kubeadm.control-plane-init = {
+      enable = true;
+      inherit token;
+    };
+  };
+  worker1 = { ... }: {
+    kubeadm.worker-join = {
+      enable = true;
+      inherit token;
+    };
+  };
+}
---
+++ configs/arianvp.me/mattermost.nix
@@ -0,0 +1,56 @@
+{
+  environment.systemPackages = [ pkgs.mattermost ];
+  services.mattermost = {
+    enable = true;
+    siteUrl = "https://mattermost.arianvp.me";
+    listenAddress = "127.0.0.1:8085";
+  };
+  services.nginx = {
+    upstreams."mattermost".servers = {
+        "${config.services.mattermost.listenAddress}" = {};
+    };
+    virtualHosts = {
+      "mattermost.arianvp.me" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."~ /api/v[0-9]+/(users/)?websocket$" = {
+          extraConfig = ''
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+            client_max_body_size 50M;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Frame-Options SAMEORIGIN;
+            proxy_buffers 256 16k;
+            proxy_buffer_size 16k;
+            client_body_timeout 60;
+            send_timeout 300;
+            lingering_timeout 5;
+            proxy_connect_timeout 90;
+            proxy_send_timeout 300;
+            proxy_read_timeout 90s;
+            proxy_pass http://mattermost;
+          '';
+        };
+        locations."/" = {
+          extraConfig = ''
+            client_max_body_size 50M;
+            proxy_set_header Connection "";
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Frame-Options SAMEORIGIN;
+            proxy_buffers 256 16k;
+            proxy_buffer_size 16k;
+            proxy_read_timeout 600s;
+            proxy_http_version 1.1;
+            proxy_pass http://mattermost;
+          '';
+        };
+      };
+    };
+  };
+}
