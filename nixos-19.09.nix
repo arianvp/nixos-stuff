79c33ffbc97f24f6f4dd174ca10328c76bbe0daa
Arian van Putten <arian.vanputten@gmail.com>
2020-04-22 18:18:01 +0200

    This file is unused

--- nixos-19.09.nix
+++
@@ -1,4 +0,0 @@
-import (fetchTarball {
-  url = "https://releases.nixos.org/nixos/19.09/nixos-19.09.1686.69ed29f5f41/nixexprs.tar.xz";
-  sha256 = "0izk28yysbiff0al24qd074rnsahhh6slp6rfl33bwarvmdnyk6y";
-})
