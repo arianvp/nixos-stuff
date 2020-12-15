59bfd70ad860aa094b20bde4ffe33c1ac748fc5d
Arian van Putten <arian@ryzen.local>
2020-12-15 10:45:07 +0100

    Keys

--- configs/ryzen/default.nix
+++ configs/ryzen/default.nix
@@ -4,25 +4,29 @@ with pkgs;
   imports = [
     ./hardware-configuration.nix
     ../../modules/yubikey
+    ../../modules/direnv.nix
   ];
 
   programs.ssh.startAgent = true;
 
   boot.kernelModules = [ "kvm-intel" "kvm-amd" ];
 
+  # :boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.kernelParams = [
+    "systemd.unified_cgroup_hierarchy=1"
+  ];
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   hardware.pulseaudio.enable = true;
 
+
   networking.hostName = "ryzen";
 
   time.timeZone = "Europe/Amsterdam";
 
-  services.kubernetes = {
-    roles = [ "master" "node" ];
-    masterAddress = "${config.networking.hostName}.local";
-  };
 
   services.avahi = {
     enable = true;
@@ -35,34 +39,37 @@ with pkgs;
       workstation = true;
     };
   };
+  fonts.fonts = [ pkgs.apl385 pkgs.noto-fonts pkgs.noto-fonts-emoji ];
+
 
   services.openssh.enable = true;
   networking.firewall.allowedTCPPorts = [ 22 ];
 
   services.xserver = {
     enable = true;
-    windowManager = {
-      xmonad = {
-        enable = true;
-        enableContribAndExtras = true;
-      };
-      default = "xmonad";
+    desktopManager.gnome3 = {
+      enable = true;
     };
-    desktopManager = { default = "none"; xterm.enable = false; };
+    #displayManager.gdm.enable = true;
+    displayManager.lightdm.enable = true;
   };
+  environment.systemPackages = [
+    pkgs.user-environment
+    pkgs.gnomeExtensions.dash-to-panel
+    pkgs.gnome3.gnome-tweaks
+    pkgs.gnome3.gnome-shell-extensions
+    pkgs.nodejs-12_x
+  ];
 
   nix.trustedUsers = [ "arian" ];
   users.extraUsers.arian = {
     isNormalUser = true;
     uid = 1000;
-    extraGroups = [ "wheel" "audio" ];
-    openssh.authorizedKeys.keyFiles = [
-      (
-        pkgs.fetchurl {
-          url = "https://github.com/arianvp.keys";
-          sha256 = "0m9qpkn0hf5cddz145yc8ws5bmbzxa296hsgzw15p3kw1ramdipq";
-        }
-      )
+    extraGroups = [ "wheel" "audio" "docker" ];
+
+    openssh.authorizedKeys.keys = [
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDXJypxL7B7Pl4WS4Suv654AguJMhYjKBPhTQNLRsBOgAAAABHNzaDo= ssh:"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICUTTzP5D0bLXRqGkw3ujx9ihqAYVC/Tz8RBz06FCeh0AAAABHNzaDo= ssh:"
     ];
   };
 
