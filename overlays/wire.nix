326253d0dd8ca65be684810a89a3057bac83baf6
Arian van Putten <arian.vanputten@gmail.com>
2020-12-19 13:31:56 +0100

    Add wire desktop internal generator.
    Use at own risk =)

--- overlays/wire.nix
+++ overlays/wire.nix
@@ -1,3 +1,14 @@
 self: super: {
-  inherit (super.channels.nixpkgs-unstable) wire-dekstop;
+  # NOTE: This is the internal development-build of wire. It is not meant for
+  # public consumption and it comes with NO warranty whatsoever
+  wire-desktop-internal = super.wire-desktop.overrideAttrs (old: old // rec {
+    version = "3.21.45-internal-45";
+    src = super.fetchurl {
+      url = "https://wire-app.wire.com/linux-internal/debian/pool/main/WireInternal-3.21.45-internal_amd64.deb";
+      sha256 = "1aj73zssj6n30n72b6kkxmgykn8vyk3ym1l8z1cd8xas948l90mb";
+    };
+    postFixup = ''
+      makeWrapper $out/opt/WireInternal/wire-desktop-internal $out/bin/wire-desktop  "''${gappsWrapperArgs[@]}"
+    '';
+  });
 }
