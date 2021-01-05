975398692431fdbcb9457ccf4f119e7f0dc5196e
Arian van Putten <arian@ryzen.local>
2021-01-05 18:47:00 +0100

    flakes: init

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "A very basic flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-20.09";
+
+  outputs = { self, nixpkgs }: {
+
+    nixosConfigurations =
+      with import nixpkgs
+        {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+          overlays = map import [
+            ./overlays/fonts.nix
+            ./overlays/wire.nix
+            ./overlays/user-environment.nix
+          ];
+        };
+      let
+        nixos' = config:
+          let deployment = nixos config; in
+          deployment // {
+            deploy = writeScript "deploy" ''
+              set -e
+              tmpDir=$(mktemp -t -d nixos-rebuild.XXXXXX)
+              SSHOPTS="$NIX_SSHOPTS -o ControlMaster=auto -o ControlPath=$tmpDir/ssh-%n -o ControlPersist=60"
+              cleanup() {
+                  for ctrl in "$tmpDir"/ssh-*; do
+                      ssh -o ControlPath="$ctrl" -O exit dummyhost 2>/dev/null || true
+                  done
+                  rm -rf "$tmpDir"
+              }
+              trap cleanup EXIT
+
+              profile=/nix/var/nix/profiles/system
+              action="$1"
+              remote="''${2:-local}"
+
+              if [ "$remote" != "local" ]; then
+                store="ssh://$remote"
+                NIX_SSHOPTS=$SSHOPTS nix copy --no-check-sigs --to "$store" "${deployment.toplevel}"
+              fi
+
+              remoteOrLocal() {
+                if [ "$remote" == "local" ]; then
+                  "$@"
+                else
+                  ssh $SSHOPTS -t "$remote" "$@"
+                fi
+              }
+              remoteOrLocal sudo nix-env --profile "$profile" --set "${deployment.toplevel}" --show-trace
+              remoteOrLocal sudo "$profile/bin/switch-to-configuration" "$action"
+            '';
+          };
+      in
+      {
+        ryzen = nixos' ./configs/ryzen;
+        t490s = nixos' ./configs/t490s;
+        arianvp-me = nixos' ./configs/arianvp-me;
+      };
+  };
+}
