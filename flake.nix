51a4732eecd06117e37b65df71ba105beb9df13f
Arian van Putten <arian.vanputten@gmail.com>
2021-12-14 17:55:02 +0100

    Add flake file

--- flake.nix
+++ flake.nix
@@ -6,29 +6,40 @@
   inputs.stable.url = "github:NixOS/nixpkgs/nixos-21.05";
   inputs.webauthn.url = "github:arianvp/webauthn-oidc";
 
-  outputs = { self, webauthn, andir, stable, unstable}: {
+  outputs = { self, webauthn, andir, stable, unstable }: {
 
-    legacyPackages = stable.legacyPackages;
     nixosModules = {
       cachix = import ./modules/cachix.nix;
       direnv = import ./modules/direnv.nix;
       systemd-initrd = import ./modules/systemd-initrd.nix;
       device-mapper = import ./modules/device-mapper.nix;
+      nixFlakes = { pkgs, ... }: {
+        nix.package = pkgs.nix_2_4;
+        nix.extraOptions = ''
+          experimental-features = nix-command flakes
+        '';
+        nix.registry.nixpkgs.flake = unstable;
+        nix.nixPath = [ "nixpkgs=${unstable}" ];
+      };
       overlays = {
-              nixpkgs.config.allowUnfree = true;
-              nixpkgs.overlays = map import [
-                ./overlays/user-environment.nix
-                ./overlays/wire.nix
-                ./overlays/fonts.nix
-                ./overlays/neovim.nix
-                ./overlays/vscodium.nix
-                ./overlays/pkgs.nix
-                # ./overlays/systemd-initrd.nix
-              ];
-            };
+        nixpkgs.config.allowUnfree = true;
+        nixpkgs.overlays = map import [
+          ./overlays/user-environment.nix
+          ./overlays/wire.nix
+          ./overlays/fonts.nix
+          ./overlays/neovim.nix
+          ./overlays/vscodium.nix
+          ./overlays/pkgs.nix
+          # ./overlays/systemd-initrd.nix
+        ];
+      };
     };
 
-    deploy.targets.node = { };
+    cachixDeploys = stable.legacyPackages.x86_64-linux.writeText "cachix-deploy.json" (builtins.toJSON {
+      agents = {
+        arianvp-me = self.nixosConfigurations.arianvp-me.config.system.build.toplevel;
+      };
+    });
 
     nixosConfigurations =
       let
