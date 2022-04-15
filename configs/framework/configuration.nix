57c40d7d4a48f2b7f619bf0507dbb7f0c881af4b
Arian van Putten <arian.vanputten@gmail.com>
2022-04-15 16:36:43 +0200

    Stuff

--- configs/framework/configuration.nix
+++ configs/framework/configuration.nix
@@ -4,8 +4,9 @@
   ];
 
   boot.initrd.verbose = false;
+  boot.initrd.systemd.enable = false;
   boot.consoleLogLevel = 0;
-  boot.kernelParams = [ "quiet" "udev.log_level=3" ];
+  boot.kernelParams = [ "udev.log_level=3" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
