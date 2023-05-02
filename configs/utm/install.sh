26f4f0ffe887ecefa085a93940c4f8e1941b7ae0
Arian van Putten <arian@mercury.com>
2023-05-02 16:25:24 +0200

    test

--- configs/utm/install.sh
+++ configs/utm/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-systemd-repart --definition ./repart.d --empty=force --dry-run=no
+systemd-repart --definition ./repart.d --empty=force --dry-run=no $1
 mount /dev/disk/by-label/root /mnt
 mount /dev/disk/by-partlabel/esp /mnt/boot
 nixos-install --flake github:arianvp/nixos-stuff
