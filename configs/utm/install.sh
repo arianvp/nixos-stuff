10f37fc7b195fb2adeb545e3b6dc5c2c55d677bd
Arian van Putten <arian@mercury.com>
2023-05-02 23:13:05 +0200

    no channel copy

--- configs/utm/install.sh
+++ configs/utm/install.sh
@@ -5,6 +5,6 @@ systemd-repart --definition ./repart.d --empty=force --dry-run=no $1
 udevadm trigger && udevadm settle
 mount /dev/disk/by-partlabel/root-arm64 /mnt
 mount /dev/disk/by-partlabel/esp /mnt/boot
-nixos-install --flake github:arianvp/nixos-stuff#utm
+nixos-install --no-channel-copy --flake github:arianvp/nixos-stuff#utm
 
 
