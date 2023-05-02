b77f0eec7d1b4fd20150a6875e318d3c071ff5d6
Arian van Putten <arian@mercury.com>
2023-05-02 16:44:48 +0200

    Fixes

--- configs/utm/install.sh
+++ configs/utm/install.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 systemd-repart --definition ./repart.d --empty=force --dry-run=no $1
+udevadm trigger && udevadm settle
 mount /dev/disk/by-partlabel/root-arm64 /mnt
 mount /dev/disk/by-partlabel/esp /mnt/boot
 nixos-install --flake github:arianvp/nixos-stuff
