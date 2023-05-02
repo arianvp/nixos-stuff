3a4938f9551cbb076d7df50978333c23f7322414
Arian van Putten <arian@mercury.com>
2023-05-03 00:12:06 +0200

    add boot-complete.target

--- configs/utm/configuration.nix
+++ configs/utm/configuration.nix
@@ -2,20 +2,21 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   # Virtualization.framework EFI driver doesnt' seem to support graphics anyway
   boot.consoleLogLevel = 3;
-  boot.kernelParams = ["quiet"];
-  boot.loader.timeout = 0; 
+  boot.kernelParams = [ "quiet" ];
+  boot.loader.timeout = 0;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.compressor = "cat";
   boot.initrd.systemd.enable = true;
   virtualisation.rosetta.enable = true;
-   services.getty.autologinUser = "arian";
+  services.getty.autologinUser = "arian";
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # Set your time zone.
@@ -24,15 +25,15 @@
   programs.nix-ld.enable = true;
   users.users.arian = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; 
+    extraGroups = [ "wheel" ];
     packages = [
       pkgs.vim
       pkgs.direnv
       pkgs.nixpkgs-fmt
     ];
-    openssh.authorizedKeys.keys = [ 
+    openssh.authorizedKeys.keys = [
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMaGVuvE+aNyuAu0E9m5scVhmnVgAutNqridbMnc261cHQwecih720LCqDwTgrI3zbMwixBuU422AK0N81DyekQ= arian@Arians-MacBook-Pro.local"
-    ];  
+    ];
   };
 
   services.openssh.enable = true;
@@ -50,5 +51,9 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
+  systemd.additionalUpstreamUnits = [
+    "boot-complete.target"
+    "systemd-boot-check-no-failures.service"
+  ];
 }
 
