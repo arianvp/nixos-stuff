e8970508ed7b6fef8413a2e97c6115f1be56b898
Arian van Putten <arian.vanputten@gmail.com>
2025-08-23 19:56:00 +0200

    spire-tpm-plugin: use fork that maybe fixes ekcert validation

--- packages/spire-tpm-plugin/package.nix
+++ packages/spire-tpm-plugin/package.nix
@@ -4,14 +4,15 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "spire-tpm-plugin";
-  version = "1.9.0";
+  version = "1.10.0pre";
   src = fetchFromGitHub {
-    owner = "spiffe";
+    owner = "arianvp";
     repo = "spire-tpm-plugin";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-vLA9ou+71bac6vB89EF21RKCGaGLbGg3gFRshgUuEWI=";
+    rev = "87464a463cfef913d11a8b472ea6005a8ed537bb";
+    # tag = "v${finalAttrs.version}";
+    hash = "sha256-kvFKq8YJpDTgTY4Wkiyn/LfqgkEl43QrYjjz3CMD32k=";
   };
-  vendorHash = "sha256-lUpCKWjgVsn0/a1rijiLzrer3rfCn5SDkBAfTYgw0aU=";
+  vendorHash = "sha256-0HkJdgIweB8SnnTsOgl3m3XL72OBhw+nBbe9GuTMY20=";
 
   # TODO: tests need cgo and go-tpm-tools/simulator which isn't working (for now?)
   doCheck = false;
