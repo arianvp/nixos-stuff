5355b73a233ddb3f3399f2d184f012e4ce0f236e
Arian van Putten <arian.vanputten@gmail.com>
2025-08-23 22:36:51 +0200

    listen on ipv6

--- configs/altra/prometheus.nix
+++ configs/altra/prometheus.nix
@@ -9,6 +9,7 @@
     "${lib.getExe' pkgs.spire "spire-agent"} api fetch x509 -socketPath $SPIFFE_ENDPOINT_SOCKET -write $RUNTIME_DIRECTORY";
   services.prometheus = {
     enable = true;
+    listenAddress = "[::]";
     webConfigFile = (pkgs.formats.yaml { }).generate "config.yml" {
       tls_server_config = {
         cert_file = "/run/prometheus/svid.0.pem";
