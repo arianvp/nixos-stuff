8a10744c93a8213a8fa4bd9554293628629d1475
Arian van Putten <arian.vanputten@gmail.com>
2025-08-23 19:55:41 +0200

    spire-controller-manager: back to upstream

--- packages/spire-controller-manager/package.nix
+++ packages/spire-controller-manager/package.nix
@@ -5,25 +5,19 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "spire-controller-manager";
-  version = "0.6.2";
+  version = "0.6.3pre";
   src = fetchFromGitHub {
     owner = "spiffe";
     repo = "spire-controller-manager";
     #rev = "v${finalAttrs.version}";
-    rev = "421205d5679befbbb9bfb8a5b03260744adedd6a";
-    hash = "sha256-kf5FRwrvGyroYVn4rWnBCWOj9OMbfCX57Qb/bjz8zW8=";
+    rev = "2128de4f24b29d48a22eae5fee2cf043cf943e6e";
+    hash = "sha256-L1QhlkTakEVXj5sPWP6rjPiDF+FL1652b4pWx+BW15s=";
   };
-  vendorHash = "sha256-1M5+D5SVWmr0pkwx7N5mfYqofO9cU0uY8ib8jYGvmlw=";
+  vendorHash = "sha256-uBfwvOFpYHwCh9f/aH3aozKyy/Z05MA/S/P+mIaIouA=";
   subPackages = [ "cmd" ];
   postInstall = ''
     mv $out/bin/cmd $out/bin/spire-controller-manager
   '';
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/spiffe/spire-controller-manager/pull/561.patch";
-      hash = "sha256-0uc14/8pa5yXII72L2Yj5StGBQFtAFYvhCTsGv+wL3Y=";
-    })
-  ];
 
   meta.mainProgram = "spire-controller-manager";
 })
