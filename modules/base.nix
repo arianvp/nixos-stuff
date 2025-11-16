180c17e769caebf02773ef046aafba6933de4f92
Arian van Putten <arian.vanputten@gmail.com>
2025-11-17 00:18:46 +0100

    remove arianvp.keys

--- modules/base.nix
+++ modules/base.nix
@@ -37,14 +37,5 @@
       pkgs.unixtools.netstat
       pkgs.claude-code
     ];
-
-    # Until we have a proper SSH-CA this is what we do instead
-    openssh.authorizedKeys.keyFiles = [
-      (pkgs.fetchurl {
-        url = "https://github.com/arianvp.keys";
-        sha256 = "sha256-HyJKxLYTQC4ZG9Xh91bCUVzkC1TBzvFkZR1XqT7aD7o=";
-      })
-    ];
   };
-
 }
