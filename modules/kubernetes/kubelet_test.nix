865641b0701c908f2e11be322e670c2374db3f6d
Arian van Putten <arian.vanputten@gmail.com>
2025-12-06 17:14:09 +0100

    Remove spire

--- modules/kubernetes/kubelet_test.nix
+++ modules/kubernetes/kubelet_test.nix
@@ -2,15 +2,13 @@
   name = "kubelet";
   nodes.foo = {
     imports = [
-      ../modules/spire/server.nix
-      ../modules/spire/agent.nix
       ./kubelet.nix
     ];
   };
 
   testScript = ''
+    foo.wait_for_unit("kubelet.service")
     foo.wait_for_unit("etcd.service")
     foo.wait_for_unit("kube-apiserver.service")
-    foo.wait_for_unit("kubelet.service")
   '';
 }
