3f6ac4b3583a751a3c2cd9aa0cdd1bf4ff9b1518
Arian van Putten <arian.vanputten@gmail.com>
2025-12-06 16:43:30 +0100

    kubelet standalone

--- modules/kubernetes/kubelet_test.nix
+++ modules/kubernetes/kubelet_test.nix
@@ -6,7 +6,7 @@
 
   testScript = ''
     foo.wait_for_unit("etcd.service")
-    foo.wait_for_unit("kube-apiserver.service")
+    # foo.wait_for_unit("kube-apiserver.service")
     foo.wait_for_unit("kubelet.service")
   '';
 }
