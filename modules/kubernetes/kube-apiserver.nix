89a5f5da56d48976e7f1f3fdae3dfa706a339ac1
Arian van Putten <arian.vanputten@gmail.com>
2025-12-07 10:53:38 +0100

    Fix things

--- modules/kubernetes/kube-apiserver.nix
+++ modules/kubernetes/kube-apiserver.nix
@@ -1,40 +1,45 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 let
   cfg = config.kubernetes.kube-apiserver;
   k8sFormats = import ./formats { inherit lib pkgs; };
-  
+
   # Generate optional config files
-  authenticationConfigFile = lib.optionalAttrs (cfg.authenticationConfig != null)
-    (k8sFormats.kubeApiserverConfigurations.authenticationConfiguration.generate 
-      "authentication-config.yaml" cfg.authenticationConfig);
-  
-  authorizationConfigFile = lib.optionalAttrs (cfg.authorizationConfig != null)
-    (k8sFormats.kubeApiserverConfigurations.authorizationConfiguration.generate 
-      "authorization-config.yaml" cfg.authorizationConfig);
-  
-  admissionConfigFile = lib.optionalAttrs (cfg.admissionConfig != null)
-    (k8sFormats.kubeApiserverConfigurations.admissionConfiguration.generate 
-      "admission-config.yaml" cfg.admissionConfig);
-  
-  encryptionConfigFile = lib.optionalAttrs (cfg.encryptionConfig != null)
-    (k8sFormats.kubeApiserverConfigurations.encryptionConfiguration.generate 
-      "encryption-config.yaml" cfg.encryptionConfig);
-  
-  tracingConfigFile = lib.optionalAttrs (cfg.tracingConfig != null)
-    (k8sFormats.kubeApiserverConfigurations.tracingConfiguration.generate 
-      "tracing-config.yaml" cfg.tracingConfig);
+  authenticationConfigFile = lib.optionalAttrs (cfg.authenticationConfig != null) (
+    k8sFormats.kubeApiserverConfigurations.authenticationConfiguration.generate "authentication-config.yaml" cfg.authenticationConfig
+  );
+
+  authorizationConfigFile = lib.optionalAttrs (cfg.authorizationConfig != null) (
+    k8sFormats.kubeApiserverConfigurations.authorizationConfiguration.generate "authorization-config.yaml" cfg.authorizationConfig
+  );
+
+  admissionConfigFile = lib.optionalAttrs (cfg.admissionConfig != null) (
+    k8sFormats.kubeApiserverConfigurations.admissionConfiguration.generate "admission-config.yaml" cfg.admissionConfig
+  );
+
+  encryptionConfigFile = lib.optionalAttrs (cfg.encryptionConfig != null) (
+    k8sFormats.kubeApiserverConfigurations.encryptionConfiguration.generate "encryption-config.yaml" cfg.encryptionConfig
+  );
+
+  tracingConfigFile = lib.optionalAttrs (cfg.tracingConfig != null) (
+    k8sFormats.kubeApiserverConfigurations.tracingConfiguration.generate "tracing-config.yaml" cfg.tracingConfig
+  );
 in
 {
   options.kubernetes.kube-apiserver = {
     enable = lib.mkEnableOption "kube-apiserver";
-    
+
     package = lib.mkOption {
       type = lib.types.package;
       default = pkgs.kubernetes;
       description = "Kubernetes package to use";
     };
-    
+
     # Configuration files
     authenticationConfig = lib.mkOption {
       type = lib.types.nullOr k8sFormats.kubeApiserverConfigurations.authenticationConfiguration.type;
@@ -44,7 +49,7 @@ in
         See https://kubernetes.io/docs/reference/config-api/apiserver-config.v1/#apiserver-k8s-io-v1-AuthenticationConfiguration
       '';
     };
-    
+
     authorizationConfig = lib.mkOption {
       type = lib.types.nullOr k8sFormats.kubeApiserverConfigurations.authorizationConfiguration.type;
       default = null;
@@ -53,7 +58,7 @@ in
         See https://kubernetes.io/docs/reference/config-api/apiserver-config.v1/#apiserver-k8s-io-v1-AuthorizationConfiguration
       '';
     };
-    
+
     admissionConfig = lib.mkOption {
       type = lib.types.nullOr k8sFormats.kubeApiserverConfigurations.admissionConfiguration.type;
       default = null;
@@ -62,7 +67,7 @@ in
         See https://kubernetes.io/docs/reference/config-api/apiserver-config.v1/#apiserver-k8s-io-v1-AdmissionConfiguration
       '';
     };
-    
+
     encryptionConfig = lib.mkOption {
       type = lib.types.nullOr k8sFormats.kubeApiserverConfigurations.encryptionConfiguration.type;
       default = null;
@@ -71,7 +76,7 @@ in
         See https://kubernetes.io/docs/reference/config-api/apiserver-config.v1/#apiserver-k8s-io-v1-EncryptionConfiguration
       '';
     };
-    
+
     tracingConfig = lib.mkOption {
       type = lib.types.nullOr k8sFormats.kubeApiserverConfigurations.tracingConfiguration.type;
       default = null;
@@ -80,11 +85,18 @@ in
         See https://kubernetes.io/docs/reference/config-api/apiserver-config.v1/#TracingConfiguration
       '';
     };
-    
+
     # Command-line arguments
     args = lib.mkOption {
-      type = lib.types.attrsOf (lib.types.oneOf [ lib.types.str lib.types.int lib.types.bool (lib.types.listOf lib.types.str) ]);
-      default = {};
+      type = lib.types.attrsOf (
+        lib.types.oneOf [
+          lib.types.str
+          lib.types.int
+          lib.types.bool
+          (lib.types.listOf lib.types.str)
+        ]
+      );
+      default = { };
       description = "Command-line arguments to pass to kube-apiserver";
       example = lib.literalExpression ''
         {
@@ -96,55 +108,29 @@ in
         }
       '';
     };
-    
-    extraArgs = lib.mkOption {
-      type = lib.types.attrsOf lib.types.str;
-      default = {};
-      description = "Additional command-line arguments";
-    };
   };
-  
+
   config = lib.mkIf cfg.enable {
     # Set defaults for kube-apiserver
-    kubernetes.kube-apiserver.args = {
-      # etcd
-      etcd-servers = lib.mkDefault "http://localhost:2379";
-      
-      # Service account configuration
-      service-account-issuer = lib.mkDefault "https://spire.nixos.sh";
-      # TODO: delegate signing to SPIRE: https://github.com/kubernetes/enhancements/tree/master/keps/sig-auth/740-service-account-external-signing
-      # service-account-signing-endpoint = "/run/signing.sock";
-      
-      # Used to verify. When unset; defaults to --tls-private-key-file
-      service-account-key-file = lib.mkDefault "/var/lib/kubernetes/sa.key";
-      # Used to sign
-      service-account-signing-key-file = lib.mkDefault "/var/lib/kubernetes/sa.key";
-      # The default; but conceptually wrong when the issuer is external.
-      # TODO: change to the cluster api server address in the future
-      api-audiences = lib.mkDefault [ "https://spire.nixos.sh" ];
-      
-      # Bind address
-      bind-address = lib.mkDefault "::";
-      # advertise-address is auto-detected
-      
-      # Service cluster IP range (must match bind-address IP family)
-      # Using IPv6 since bind-address defaults to "::"
-      service-cluster-ip-range = lib.mkDefault "fd00:10:96::/112";
-      
-      # TODO: Authentication
-      # TODO: Authorization
-    } // lib.optionalAttrs (cfg.authenticationConfig != null) {
-      authentication-config = authenticationConfigFile;
-    } // lib.optionalAttrs (cfg.authorizationConfig != null) {
-      authorization-config = authorizationConfigFile;
-    } // lib.optionalAttrs (cfg.admissionConfig != null) {
-      admission-control-config-file = admissionConfigFile;
-    } // lib.optionalAttrs (cfg.encryptionConfig != null) {
-      encryption-provider-config = encryptionConfigFile;
-    } // lib.optionalAttrs (cfg.tracingConfig != null) {
-      tracing-config-file = tracingConfigFile;
-    };
-    
+    kubernetes.kube-apiserver.args =
+      {
+      }
+      // lib.optionalAttrs (cfg.authenticationConfig != null) {
+        authentication-config = authenticationConfigFile;
+      }
+      // lib.optionalAttrs (cfg.authorizationConfig != null) {
+        authorization-config = authorizationConfigFile;
+      }
+      // lib.optionalAttrs (cfg.admissionConfig != null) {
+        admission-control-config-file = admissionConfigFile;
+      }
+      // lib.optionalAttrs (cfg.encryptionConfig != null) {
+        encryption-provider-config = encryptionConfigFile;
+      }
+      // lib.optionalAttrs (cfg.tracingConfig != null) {
+        tracing-config-file = tracingConfigFile;
+      };
+
     systemd.services.kube-apiserver = {
       wantedBy = [ "multi-user.target" ];
       # Fixes: Unable to find suitable network address.error='no default routes
@@ -152,13 +138,13 @@ in
       # AdvertiseAddress directly or provide a valid BindAddress to fix this.
       requires = [ "network-online.target" ];
       after = [ "network-online.target" ];
-      
+
       serviceConfig = {
         Type = "notify";
         WatchdogSec = "30s";
         RuntimeDirectory = "kubernetes";
         StateDirectory = "kubernetes";
-        
+
         # TODO: Key persistence or out-source to SPIRE
         ExecStartPre = lib.mkIf (cfg.args.service-account-key-file == "/var/lib/kubernetes/sa.key") (
           pkgs.writeShellScript "generate-sa-key" ''
@@ -168,13 +154,13 @@ in
             fi
           ''
         );
-        
-        ExecStart = 
+
+        ExecStart =
           let
-            args = lib.cli.toGNUCommandLineShell {} (cfg.args // cfg.extraArgs);
+            args = lib.cli.toGNUCommandLineShell { } cfg.args;
           in
           "${lib.getExe' cfg.package "kube-apiserver"} ${args}";
-        
+
         Restart = "on-failure";
         RestartSec = "5s";
       };
--- modules/kubernetes/kubernetes.nix
+++ modules/kubernetes/kubernetes.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ lib, config, ... }:
 {
   imports = [
     ./etcd.nix
@@ -13,6 +13,38 @@
   systemd.services.kubelet = {
     after = [ "crio.service" ];
     requires = [ "crio.service" ];
+  };
+
+  kubernetes.kube-apiserver = {
+    args = {
+      # etcd
+      etcd-servers = ["http://127.0.0.1:2379"];
+
+      # Service account configuration
+      service-account-issuer = "https://spire.nixos.sh";
+      # TODO: delegate signing to SPIRE: https://github.com/kubernetes/enhancements/tree/master/keps/sig-auth/740-service-account-external-signing
+      # service-account-signing-endpoint = "/run/signing.sock";
+
+      # Used to verify. When unset; defaults to --tls-private-key-file
+      service-account-key-file = "/var/lib/kubernetes/sa.key";
+      # Used to sign
+      service-account-signing-key-file = "/var/lib/kubernetes/sa.key";
+
+      # The default; but conceptually wrong when the issuer is external.
+      # TODO: change to the cluster api server address in the future
+      api-audiences = [ "https://spire.nixos.sh" ];
+
+      # Bind address
+      bind-address = lib.mkDefault "::";
+      # advertise-address is auto-detected
+
+      # Service cluster IP range (must match bind-address IP family)
+      # Using IPv6 since bind-address defaults to "::"
+      service-cluster-ip-range = lib.mkDefault "fd00:10:96::/112";
+
+      # TODO: Authentication
+      # TODO: Authorization
+    };
   };
 
   # Set defaults for kubelet configuration
