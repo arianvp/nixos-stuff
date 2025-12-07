2b91b0c8b3adee0ef89ad9e93f67a20638142f4a
Arian van Putten <arian.vanputten@gmail.com>
2025-12-07 17:35:08 +0100

    Crio tests work

---
+++ modules/kubernetes/images/gcr.io/k8s-staging-cri-tools/test-image-user-username-group-latest/default.nix
@@ -0,0 +1,7 @@
+{
+  imageName = "gcr.io/k8s-staging-cri-tools/test-image-user-username-group";
+  imageDigest = "sha256:e5f6d69e12c3388e8e945dbbd3ed0643cc1df7ecbe0eb01717bfff99917ae04b";
+  hash = "sha256-sOJN4sozkcSvrjIs7cOFfmz/Uwwpx5oBFG4qoTDatew=";
+  finalImageName = "gcr.io/k8s-staging-cri-tools/test-image-user-username-group";
+  finalImageTag = "latest";
+}
