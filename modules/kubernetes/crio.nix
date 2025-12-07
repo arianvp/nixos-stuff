005ef3c35eeceb867cbbb20f0dc562e56c132499
Arian van Putten <arian.vanputten@gmail.com>
2025-12-07 12:55:59 +0100

    Try crio it

--- modules/kubernetes/crio.nix
+++ modules/kubernetes/crio.nix
@@ -1,10 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  pause = pkgs.dockerTools.pullImage (import ./images/pause.nix);
+in
 {
   # TODO: Upstream sets watchdog. NixOS doesn't.
   # Again the problem is that NixOS sucks
   virtualisation.containers.enable = true;
   virtualisation.cri-o.enable = true;
   virtualisation.podman.enable = true;
+
+  systemd.services.podman-load-pause = {
+    wantedBy = [ "crio.service" ];
+    before = [ "crio.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      StandardInput = "file:${pause}";
+      ExecStart = "${pkgs.podman}/bin/podman load";
+    };
+  };
 
 }
