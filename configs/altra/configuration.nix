793626f1e0d1cc8552b0064b4076a3f6357ad94e
Arian van Putten <arian.vanputten@gmail.com>
2025-12-15 23:17:50 +0100

    operators

--- configs/altra/configuration.nix
+++ configs/altra/configuration.nix
@@ -116,17 +116,73 @@
       receivers.journald = {
         # TODO: Cursor
         directory = "/var/log/journal";
+        operators = [
+          # Map syslog PRIORITY to OTel severity (0=emerg...7=debug)
+          {
+            type = "severity_parser";
+            parse_from = "attributes.PRIORITY";
+            preset = "none";
+            mapping = {
+              fatal = [ "0" "1" "2" ];    # emerg, alert, crit
+              error = [ "3" ];             # err
+              warn = [ "4" ];              # warning
+              info = [ "5" "6" ];          # notice, info
+              debug = [ "7" ];             # debug
+            };
+          }
+
+          # Map systemd unit to service.name (prefer UNIT over SYSLOG_IDENTIFIER)
+          { type = "copy"; from = "attributes.UNIT"; to = "resource[\"service.name\"]"; }
+
+          # Map host information to resource attributes
+          { type = "move"; from = "attributes._HOSTNAME"; to = "resource[\"host.name\"]"; }
+          { type = "move"; from = "attributes._MACHINE_ID"; to = "resource[\"host.id\"]"; }
+
+          # Map process information to OTel semantic conventions
+          { type = "move"; from = "attributes._PID"; to = "attributes[\"process.pid\"]"; }
+          { type = "move"; from = "attributes._EXE"; to = "attributes[\"process.executable.path\"]"; }
+          { type = "move"; from = "attributes._CMDLINE"; to = "attributes[\"process.command_line\"]"; }
+
+          # Map source code location to OTel semantic conventions
+          { type = "move"; from = "attributes.CODE_FILE"; to = "attributes[\"code.filepath\"]"; }
+          { type = "move"; from = "attributes.CODE_FUNC"; to = "attributes[\"code.function\"]"; }
+          { type = "move"; from = "attributes.CODE_LINE"; to = "attributes[\"code.lineno\"]"; }
+
+          # Keep systemd-specific fields in a namespace
+          { type = "copy"; from = "attributes.UNIT"; to = "attributes[\"systemd.unit\"]"; }
+          { type = "move"; from = "attributes.INVOCATION_ID"; to = "attributes[\"systemd.invocation_id\"]"; }
+          { type = "move"; from = "attributes._SYSTEMD_CGROUP"; to = "attributes[\"systemd.cgroup\"]"; }
+          { type = "move"; from = "attributes._SYSTEMD_SLICE"; to = "attributes[\"systemd.slice\"]"; }
+
+          # Remove original/redundant journal fields
+          { type = "remove"; field = "attributes.UNIT"; }
+          { type = "remove"; field = "attributes.SYSLOG_IDENTIFIER"; }
+          { type = "remove"; field = "attributes.PRIORITY"; }
+          { type = "remove"; field = "attributes.SYSLOG_FACILITY"; }
+          { type = "remove"; field = "attributes.MESSAGE_ID"; }
+          { type = "remove"; field = "attributes._UID"; }
+          { type = "remove"; field = "attributes._GID"; }
+          { type = "remove"; field = "attributes.TID"; }
+          { type = "remove"; field = "attributes._COMM"; }
+          { type = "remove"; field = "attributes._CAP_EFFECTIVE"; }
+          { type = "remove"; field = "attributes._BOOT_ID"; }
+          { type = "remove"; field = "attributes._TRANSPORT"; }
+          { type = "remove"; field = "attributes._RUNTIME_SCOPE"; }
+          { type = "remove"; field = "attributes._SOURCE_REALTIME_TIMESTAMP"; }
+          { type = "remove"; field = "attributes.__CURSOR"; }
+          { type = "remove"; field = "attributes.__MONOTONIC_TIMESTAMP"; }
+          { type = "remove"; field = "attributes.__SEQNUM"; }
+          { type = "remove"; field = "attributes.__SEQNUM_ID"; }
+        ];
       };
       service = {
         pipelines = {
           logs = {
             receivers = [ "journald" ];
             exporters = [ "otlp" ];
-
           };
         };
       };
-
     };
   };
 }
