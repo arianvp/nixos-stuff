ab78a0b50eb250d6ba3ddb0487ac639b438f54b8
Arian van Putten <arian.vanputten@gmail.com>
2025-12-19 12:40:15 +0100

    Add mappigns

--- modules/opentelemetry-collector.nix
+++ modules/opentelemetry-collector.nix
@@ -64,8 +64,42 @@
           metric_statements = [{
             context = "datapoint";
             statements = [
-              "set(attributes[\"deployment.environment\"], resource.attributes[\"deployment.environment\"])"
-              "set(attributes[\"service.version\"], resource.attributes[\"service.version\"])"
+              ''set(attributes["deployment.environment"], resource.attributes["deployment.environment"])''
+              ''set(attributes["service.version"], resource.attributes["service.version"])''
+            ];
+          }];
+        };
+        "transform/journal_semantic_conventions" = {
+          error_mode = "ignore";
+          log_statements = [{
+            context = "log";
+            statements = [
+              # Unique identifier
+              ''set(attributes["log.record.uid"], body["__CURSOR"])''
+
+              # Process attributes
+              ''set(attributes["process.pid"], body["_PID"])''
+              ''set(attributes["process.executable.path"], body["_EXE"])''
+              ''set(attributes["process.executable.name"], body["_COMM"])''
+              ''set(attributes["process.command_line"], body["_CMDLINE"])''
+              ''set(attributes["thread.id"], body["TID"])''
+
+              # Host/System attributes
+              ''set(attributes["host.name"], body["_HOSTNAME"])''
+              ''set(attributes["host.id"], body["_MACHINE_ID"])''
+
+              # Service/Unit attributes
+              ''set(attributes["service.name"], body["_SYSTEMD_UNIT"])''
+              ''set(attributes["service.instance.id"], body["_SYSTEMD_INVOCATION_ID"])''
+              ''set(attributes["process.linux.cgroup"], body["_SYSTEMD_CGROUP"])''
+
+              # Log severity
+              ''set(attributes["log.severity_number"], body["PRIORITY"])''
+
+              # Code location attributes
+              ''set(attributes["code.filepath"], body["CODE_FILE"])''
+              ''set(attributes["code.function"], body["CODE_FUNC"])''
+              ''set(attributes["code.lineno"], body["CODE_LINE"])''
             ];
           }];
         };
@@ -108,7 +142,7 @@
           # Logs pipeline
           logs = {
             receivers = [ "otlp" "journald" ];
-            processors = [ "resourcedetection" "batch" ];
+            processors = [ "resourcedetection" "transform/journal_semantic_conventions" "batch" ];
             exporters = [ "otlp/honeycomb" "otlphttp/grafana_cloud" ];
           };
         };
