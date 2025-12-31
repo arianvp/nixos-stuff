3bffc77c2e6d27366d4c9c3a56cc3b2d5672a1e1
Arian van Putten <arian.vanputten@gmail.com>
2025-12-31 14:25:36 +0100

    modernize journald-receiver

--- modules/opentelemetry-collector/journald-receiver.nix
+++ modules/opentelemetry-collector/journald-receiver.nix
@@ -1,13 +1,4 @@
 { lib, ... }:
-let
-  copy = from: to: {
-    type = "copy";
-    inherit from to;
-    "if" = "${from} != nil";
-  };
-  copyAttr = from: to: copy from ''attributes["${to}"]'';
-  copyResource = from: to: copy from ''resource["${to}"]'';
-in
 {
   services.opentelemetry-collector.settings = {
     extensions."file_storage/journald" = {
@@ -18,146 +9,126 @@ in
       directory = "/var/log/journal";
       storage = "file_storage/journald";
       start_at = "end";
-      operators = [
-        # Log-level attributes (not resource attributes)
-        (copyAttr "body.__CURSOR" "log.record.uid")
-        {
-          type = "severity_parser";
-          parse_from = "body.PRIORITY";
-          overwrite_text = true;
-          mapping = {
-            debug = {
-              min = 7;
-              max = 7;
-            };
-            info = {
-              min = 6;
-              max = 6;
-            };
-            info2 = {
-              min = 5;
-              max = 5;
-            };
-            warn = {
-              min = 4;
-              max = 4;
-            };
-            error = {
-              min = 3;
-              max = 3;
-            };
-            error2 = {
-              min = 2;
-              max = 2;
-            };
-            error3 = {
-              min = 1;
-              max = 1;
-            };
-            fatal = {
-              min = 0;
-              max = 0;
-            };
+      operators =
+
+        let
+          moveIfNotNil = from: to: {
+            type = "move";
+            inherit from to;
+            "if" = "${from} != nil";
           };
-          "if" = "body.PRIORITY != nil";
-        }
+          moveFromAttr = fromAttr: moveIfNotNil ''attributes["${fromAttr}"'';
+          # moveToAttr = from: to: moveIfNotNil from ''attributes["${to}"]'';
+          moveAttr = from: to: moveFromAttr from ''attributes["${to}"]'';
+          moveResource = from: to: moveFromAttr from ''resource["${to}"]'';
+        in
 
-        (copyAttr "body.CODE_FILE" "code.filepath")
-        (copyAttr "body.CODE_FUNC" "code.function")
-        (copyAttr "body.CODE_LINE" "code.lineno")
-
-        (copyAttr "body.TID" "thread.id")
-
-        (copyAttr "body._TRANSPORT" "log.iostream")
-        (copyAttr "body._STREAM_ID" "systemd.stream.id")
-
-        # The message type
-        # TODO: SemConv equiv?
-        (copyAttr "body.MESSAGE_ID" "systemd.message.id")
-        (copyAttr "body.ERRNO" "systemd.errno")
-
-        # systemd
-        (copyAttr "body.INVOCATION_ID" "systemd.invocation.id")
-        (copyAttr "body.USER_INVOCATION_ID" "systemd.user.invocation.id")
-        (copyAttr "body.UNIT" "systemd.unit")
-        (copyAttr "body.USER_UNIT" "systemd.user.unit")
-
-        # TODO: find semantic variant
-        (copyAttr "body.DOCUMENTATION" "systemd.documentation")
-
-        # delegated
-        (copyAttr "body.OBJECT_PID" "process.pid")
-        (copyAttr "body.OBJECT_CWD" "process.working_directory")
-        (copyAttr "body.OBJECT_EXE" "process.executable.path")
-        (copyAttr "body.OBJECT_CMDLINE" "process.command_line")
-        (copyAttr "body.OBJECT_UID" "process.user.id")
-        (copyAttr "body.OBJECT_GID" "process.group.id")
-        (copyAttr "body.OBJECT_SYSTEMD_CGROUP" "process.linux.cgroup")
-        # made this one up
-        (copyAttr "body.OBJECT_CAP_EFFECTIVE" "process.capabilities.effective")
-        (copyAttr "body.OBJECT_SYSTEMD_UNIT" "systemd.unit")
-        (copyAttr "body.OBJECT_SYSTEMD_SLICE" "systemd.slice")
-        (copyAttr "body.OBJECT_SYSTEMD_INVOCATION_ID" "systemd.invocation.id")
-
-        (copyAttr "body.COREDUMP_PID" "process.pid")
-        (copyAttr "body.COREDUMP_CWD" "process.working_directory")
-        (copyAttr "body.COREDUMP_COMM" "process.executable.name")
-        (copyAttr "body.COREDUMP_EXE" "process.executable.path")
-        (copyAttr "body.COREDUMP_CMDLINE" "process.command_line")
-        (copyAttr "body.COREDUMP_UID" "process.user.id")
-        (copyAttr "body.COREDUMP_GID" "process.group.id")
-        (copyAttr "body.COREDUMP_CGROUP" "process.linux.cgroup")
-        (copyAttr "body.COREDUMP_UNIT" "systemd.unit")
-
-        (copyAttr "body.COREDUMP_SLICE" "systemd.slice")
-
-        # COREDUMP_SIGNAL_NAME
-        # COREDUMP_SIGNAL
-        # COREDUMP_CONTAINER_CMDLINE
-        # COREDUMP_HOSTNAME
-        # COREDUMP_TIMESTAMP
-        # NOTE: Bit of a bitch to map to https://opentelemetry.io/docs/specs/semconv/registry/attributes/process/#process-environment-variable due to it not being a map
-        # COREDUMP_ENVIRON
-
-        # Resource attributes - process
-        (copyResource "body._PID" "process.pid")
-        (copyResource "body._UID" "process.user.id")
-        (copyResource "body._GID" "process.group.id")
-        (copyResource "body._EXE" "process.executable.path")
-        (copyResource "body._COMM" "process.executable.name")
-        (copyResource "body._CMDLINE" "process.command_line")
-        (copyResource "body._CAP_EFFECTIVE" "process.capabilities.effective")
-        (copyResource "body._SYSTEMD_CGROUP" "process.linux.cgroup")
-
-        (copyResource "body._SYSTEMD_CGROUP" "systemd.cgroup")
-        (copyResource "body._SYSTEMD_SLICE" "systemd.slice")
-        (copyResource "body._SYSTEMD_INVOCATION_ID" "systemd.invocation_id")
-        (copyResource "body._SYSTEMD_UNIT" "systemd.unit")
-
-        # TODO: Do we actually set service.name? And should we set it to SYSLOG_IDENTIFIER?
-
-        /*
-          (copyResource "body._SYSTEMD_UNIT" "service.name")
+        [
+          # from https://www.dash0.com/guides/opentelemetry-journald-receiver
           {
             type = "move";
-            from = "body.SYSLOG_IDENTIFIER";
-            to = ''resource["service.name"]'';
-            "if" = ''body.SYSLOG_IDENTIFIER != nil && resource["service.name"] == nil'';
+            from = "body";
+            to = ''attributes["body"]'';
           }
-          (copyResource "body._SYSTEMD_INVOCATION_ID" "service.instance.id")
-        */
+          {
+            type = "copy";
+            from = ''attributes["body"]'';
+            to = ''attributes["log.record.original"]'';
+          }
+          {
+            type = "flatten";
+            field = ''attributes["body"]'';
+          }
+          {
+            type = "move";
+            from = ''attributes["MESSAGE"]'';
+            to = "body";
+          }
+          {
+            type = "severity_parser";
+            parse_from = ''attributes["PRIORITY"]'';
+            overwrite_text = true;
+            mapping = {
+              debug = 7;
+              info = 6;
+              info2 = 5;
+              warn = 4;
+              error = 3;
+              error2 = 2;
+              error3 = 1;
+              fatal = 0;
+            };
+            "if" = ''attributes["PRIORITY"] != nil'';
+          }
+          (moveAttr "__CURSOR" "log.record.uid")
+          (moveAttr "CODE_FILE" "code.filepath")
+          (moveAttr "CODE_FUNC" "code.function")
+          (moveAttr "CODE_LINE" "code.lineno")
+          (moveAttr "TID" "thread.id")
 
-        {
-          type = "move";
-          from = "body";
-          to = ''attributes["log.record.original"]'';
-        }
-        {
-          type = "copy";
-          from = ''attributes["log.record.original"]["MESSAGE"]'';
-          to = "body";
-        }
-      ];
+          (moveAttr "_TRANSPORT" "log.iostream")
+          (moveAttr "_STREAM_ID" "systemd.stream.id")
+
+          # The message type
+          # TODO: SemConv equiv?
+          (moveAttr "MESSAGE_ID" "systemd.message.id")
+          (moveAttr "ERRNO" "systemd.errno")
+
+          # systemd
+          (moveAttr "INVOCATION_ID" "systemd.invocation.id")
+          (moveAttr "USER_INVOCATION_ID" "systemd.user.invocation.id")
+          (moveAttr "UNIT" "systemd.unit")
+
+          # TODO: find semantic variant
+          (moveAttr "DOCUMENTATION" "systemd.documentation")
+
+            # delegated
+            (moveAttr "OBJECT_PID" "process.pid")
+            (moveAttr "OBJECT_CWD" "process.working_directory")
+            (moveAttr "OBJECT_EXE" "process.executable.path")
+            (moveAttr "OBJECT_CMDLINE" "process.command_line")
+            (moveAttr "OBJECT_UID" "process.user.id")
+            (moveAttr "OBJECT_GID" "process.group.id")
+            (moveAttr "OBJECT_SYSTEMD_CGROUP" "process.linux.cgroup")
+            (moveAttr "OBJECT_CAP_EFFECTIVE" "process.capabilities.effective")
+            (moveAttr "OBJECT_SYSTEMD_UNIT" "systemd.unit")
+            (moveAttr "OBJECT_SYSTEMD_SLICE" "systemd.slice")
+            (moveAttr "OBJECT_SYSTEMD_INVOCATION_ID" "systemd.invocation.id")
+
+            # coredump-delegated
+            (moveAttr "COREDUMP_PID" "process.pid")
+            (moveAttr "COREDUMP_CWD" "process.working_directory")
+            (moveAttr "COREDUMP_COMM" "process.executable.name")
+            (moveAttr "COREDUMP_EXE" "process.executable.path")
+            (moveAttr "COREDUMP_CMDLINE" "process.command_line")
+            (moveAttr "COREDUMP_UID" "process.user.id")
+            (moveAttr "COREDUMP_GID" "process.group.id")
+            (moveAttr "COREDUMP_CGROUP" "process.linux.cgroup")
+            (moveAttr "COREDUMP_UNIT" "systemd.unit")
+            (moveAttr "COREDUMP_SLICE" "systemd.slice")
+
+            # TODO: Bit of a bitch to map to
+            # https://opentelemetry.io/docs/specs/semconv/registry/attributes/process/#process-environment-variable
+            # due to it not being a map
+            # COREDUMP_ENVIRON
+
+          # Resource attributes - process
+          (moveResource "_PID" "process.pid")
+          (moveResource "_UID" "process.user.id")
+          (moveResource "_GID" "process.group.id")
+          (moveResource "_EXE" "process.executable.path")
+          (moveResource "_COMM" "process.executable.name")
+          (moveResource "_CMDLINE" "process.command_line")
+          (moveResource "_CAP_EFFECTIVE" "process.capabilities.effective")
+          (moveResource "_SYSTEMD_CGROUP" "process.linux.cgroup")
+
+          # TODO: What to do with SYSLOG_IDENTIFIER?
+
+          (moveResource "_SYSTEMD_CGROUP" "systemd.cgroup")
+          (moveResource "_SYSTEMD_SLICE" "systemd.slice")
+          (moveResource "_SYSTEMD_INVOCATION_ID" "systemd.invocation_id")
+          (moveResource "_SYSTEMD_UNIT" "systemd.unit")
+        ];
     };
   };
 
