f5e087110283acb1c5058abd781ea9ab1b56f53e
Arian van Putten <arian@arianvp.me>
2026-02-26 22:53:22 +0100

    Flake

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -42,7 +42,7 @@
         # TODO: point to whatever yubikey is inserted. I wonder if we can do this with udev
         key = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJfMczLTlN0csX8HFFneaYW6OH0lupwCryoDANaqR6lxAAAABHNzaDo=";
       };
-      git.sign-on-push = true;
+      # git.sign-on-push = true;
     };
   };
   home.stateVersion = "26.05";
