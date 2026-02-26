5727d417740bc8588e1de565e6ff92a1762bf03e
Arian van Putten <arian@arianvp.me>
2026-02-27 00:42:36 +0100

    ssh-keygen

--- keys/generate.sh
+++ keys/generate.sh
@@ -2,15 +2,23 @@
 
 set -euo pipefail
 
+
+# scope is the purpose of the key. empty scope is just your normal generic ssh key
+# example scopes : git , ca,  sign,
+# user is when you have multiple instances of the same purpose. e.g. if you have multiple identities.
+# empty user is the "default" identity.  Useful if you have both "work" and non-work identities
+
 scope="${1:-}"
 user="${2:-}"
 application="ssh:$scope"
-comment="$application"
+comment="${user:+$user@}$application"
 
+# TODO: I might wanna do configurable resident keys in the future
+resident=1
 
+# same naming convention that `ssh-keygen -K` uses. For some reason ssh-keygen and ssh-keygen -K are not aligned
 suffix="${scope:+_${scope}}${user:+_${user}}"
-
-file="id_ed25519_sk_${resident:+rk}${scope:+_${scope}}${user:+_${user}}"
+file="id_ed25519_sk${resident:+_rk}${scope:+_${scope}}${user:+_${user}}"
 
 ssh-keygen -t ed25519-sk \
   -O resident \
@@ -21,6 +29,3 @@ ssh-keygen -t ed25519-sk \
   -N "" \
   -f "$file" \
   -O write-attestation="${file}.att"
-
-
-
