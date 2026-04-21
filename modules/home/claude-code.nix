e32a220c9ab70a9ca87ebf7963cf2fcae6e18614
Arian van Putten <arian@arianvp.me>
2026-04-21 12:25:09 +0200

    claude sandbox

--- modules/home/claude-code.nix
+++ modules/home/claude-code.nix
@@ -3,6 +3,31 @@
   programs.claude-code = {
     enable = true;
     settings = {
+      sandbox = {
+        enabled = true;
+        failIfUnavailable = true;
+        filesystem = {
+          denyWrite = [ "/" ];
+          denyRead = [ "/" ];
+          allowRead = [
+            "."
+            "/nix/store"
+            "/nix/var/nix/daemon-socket/socket"
+          ];
+          allowWrite = [
+            "."
+            "/nix/var/nix/daemon-socket/socket"
+          ];
+        };
+        network = {
+          allowUnixSockets = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin [ "/nix/var/nix/daemon-socket/socket" ];
+          allowAllUnixSockets = pkgs.stdenv.hostPlatform.isLinux;
+          allowedDomains = [
+            "cache.nixos.org"
+            "channels.nixos.org"
+          ];
+        };
+      };
       permissions = {
         deny = [ "Bash(git *)" ];
         allow = [
