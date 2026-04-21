322d6a1a40c46bb4972ec93da7b2353eb1b9c21a
Arian van Putten <arian@arianvp.me>
2026-04-21 12:25:09 +0200

    altra sshfs

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -1,7 +1,10 @@
 { lib, pkgs, ... }:
 {
 
-  imports = [ ./jj ./claude-code.nix ];
+  imports = [
+    ./jj
+    ./claude-code.nix
+  ];
 
   programs.noctalia-shell = {
     systemd.enable = true;
@@ -13,6 +16,19 @@
   programs.direnv.enable = true;
 
   home.packages = [ pkgs.claude-code ];
+
+  systemd.user = {
+    automounts.altra = {
+      Automount.Where = "%h/altra";
+      Install.WantedBy = [ "default.target" ];
+    };
+    mounts.altra.Mount = {
+      Where = "%h/altra";
+      What = "arian@altra.ygg.nixos.sh:/home/arian";
+      Type = "sshfs";
+      Options = "reconnect,ServerAliveInterval=15,dir_cache=yes,idmap=user,follow_symlinks,transform_symlinks,compression=yes";
+    };
+  };
 
   home.stateVersion = "26.05";
 }
