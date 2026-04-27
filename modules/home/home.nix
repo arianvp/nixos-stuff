80f007f19279ff360da855758023f91fc4943ae6
Arian van Putten <arian@arianvp.me>
2026-04-27 11:45:11 +0200

    sshfs

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -1,4 +1,9 @@
-{ lib, pkgs, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 {
 
   imports = [
@@ -34,18 +39,28 @@
 
     programs.direnv.enable = true;
 
-    home.packages = [ pkgs.claude-code pkgs.sshfs ];
+    home.packages = [
+      pkgs.claude-code
+      pkgs.sshfs
+    ];
 
     systemd.user = {
+      # TODO: Seems automounts don't work unpriveleged
+      # Apr 27 11:43:27 framework systemd[2194]: Failed to mount systemd-2194 (type autofs) on /home/arian/altra (0 "fd=98,pgrp=2194,minproto=5,maxproto=5,direct"): Operation not permitted
+      # Apr 27 11:43:27 framework systemd[2194]: home-arian-altra.automount: Failed with result 'resources'.
+      # Apr 27 11:43:27 framework systemd[2194]: Failed to set up automount home-arian-altra.automount.
       automounts.home-arian-altra = {
         Automount.Where = "/home/arian/altra";
         Install.WantedBy = [ "default.target" ];
       };
-      mounts.home-arian-altra.Mount = {
-        Where = "/home/arian/altra";
-        What = "arian@altra.ygg.nixos.sh:/home/arian";
-        Type = "fuse.sshfs";
-        Options = "_netdev,user,delay_connect,reconnect,ServerAliveInterval=15,dir_cache=yes,idmap=user,follow_symlinks,transform_symlinks,compression=yes";
+      mounts.home-arian-altra = {
+        Install.WantedBy = [ "default.target" ];
+        Mount = {
+          Where = "/home/arian/altra";
+          What = "arian@altra.ygg.nixos.sh:/home/arian";
+          Type = "fuse.sshfs";
+          Options = "_netdev,user,delay_connect,reconnect,ServerAliveInterval=15,dir_cache=yes,idmap=user,follow_symlinks,transform_symlinks,compression=yes";
+        };
       };
     };
 
