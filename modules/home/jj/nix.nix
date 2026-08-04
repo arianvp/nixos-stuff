819ff2804adfbfe820d678ab5dd5b16e20adf937
Arian van Putten <arian@arianvp.me>
2026-08-04 10:43:09 +0200

    fix: Warning: Bare invocation of nixfmt is deprecated. Use 'nixfmt -' for anonymous stdin.

--- modules/home/jj/nix.nix
+++ modules/home/jj/nix.nix
@@ -1,13 +1,20 @@
-{pkgs, lib, ...}:
+{ pkgs, lib, ... }:
 {
   programs.jujutsu.settings.fix.tools = {
     "20-nixfmt" = {
-      command = [ (lib.getExe pkgs.nixfmt) ];
+
+      command = [
+        (lib.getExe pkgs.nixfmt)
+        "-"
+      ];
       patterns = [ "glob:'**/*.nix'" ];
     };
     # useful for nixpkgs conflicts. runs in treefmt in nixpkgs too
     "80-keep-sorted" = {
-      command = [ (lib.getExe pkgs.keep-sorted) "-" ];
+      command = [
+        (lib.getExe pkgs.keep-sorted)
+        "-"
+      ];
       patterns = [ "glob:'**/*'" ];
     };
     "00-statix" = {
